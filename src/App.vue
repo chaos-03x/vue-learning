80877aa7278b60956f4f1727aa0ea206099d6a3d
chaos-03x <a131139899@gmail.com>
2025-05-30 20:13:36 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
     <div>
         <h1>Hello World</h1>
-        <Person_vue2/>   
+        <!-- <Person_vue2/>   
         <Person_vue3/>   
-        <vfor/>
+        <vfor/> -->
+        <Computed/>
     </div>
     
 </template>
@@ -12,9 +13,10 @@
 import Person_vue2 from './components/Person_vue2.vue'
 import Person_vue3 from './components/Person_vue3.vue'
 import vfor from './components/V-for.vue'
+import Computed from './components/Computed.vue'
 
 export default {
-    components: {Person_vue2,Person_vue3,vfor}
+    components: {Person_vue2,Person_vue3,vfor,Computed}
 }
 </script>
 
---
+++ src/components/Computed.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="box">
+        <h1>Computed计算属性</h1>
+        <!-- v-bind单向绑定，v-model双向绑定 -->
+        姓：<input type="text" v-model="firstName"> <br>
+        名：<input type="text" v-model="lastName"> <br>
+        全名：<span>{{ firstName }}-{{ lastName }}</span>
+    </div>
+</template>
+
+<script setup>
+defineOptions({ name: "Computed" })
+import {ref} from 'vue'
+
+let firstName = ref("张")
+let lastName = ref("三")
+let fullName = ref("张三")
+</script>
+
+<style scoped>
+#box {
+    background-color: rgb(177, 128, 206);
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+
+button {
+    margin: 0 5px;
+    border-radius: 5px;
+}
+</style>
