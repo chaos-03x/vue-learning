f1c3a7af75d6916f31f74b1545d2c46253fcafa9
chaos-03x <a131139899@gmail.com>
2025-06-14 14:52:40 +0800

    update

---
+++ src/components/Lifecycle.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Vue3的生命周期</h1>
+</template>
+
+<script setup> // 创建阶段在setup完成
+import { ref,onBeforeMount,onBeforeUnmount,onBeforeUpdate,
+onUpdated,onBeforeUnmount,onUnmounted } from "vue";
+
+
+</script>
+
+<style scoped>
+</style>
